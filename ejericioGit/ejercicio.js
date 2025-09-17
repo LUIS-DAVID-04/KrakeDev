21b1aaaf712dba8d4f9f7a6feb8e27ae099ede2f
LUIS-DAVID-04 <floreshurtadoluisdavid9@gmail.com>
2025-09-17 17:20:06 -0500

    El boton cambia la imagen

--- ejericioGit/ejercicio.js
+++ ejericioGit/ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImgSaludo;
+    cmpImgSaludo=document.getElementById("saludo");
+    cmpImgSaludo.src="./Imagenes/trofeo.gif"
     console.log("Ingresa a saludar");
+
 }
