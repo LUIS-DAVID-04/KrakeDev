3e65b5c39df70229b74feb9d60af79a781fddbf4
LUIS-DAVID-04 <floreshurtadoluisdavid9@gmail.com>
2025-09-22 10:18:30 -0500

    Implementacion de int y float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,14 +1,29 @@
 
-saludar=function(){
+saludar = function(){
     //Recuperar el valor de la caja de texto txtNombre
     let nombre=recuperarTexto("txtNombre");
     //Recuperar el valor de la caja de texto txtApellido
     let Apellido=recuperarTexto("txtApellido");
+    //Recuperar el valor de la caja de texto txtEdad
+    let edad = recuperarInt("txtedad");
+    //Recuperar el valor de la caja de texto txtEstatura 
+    let estatura = recuperarFloat("txtestatura");
 }
-recuperarTexto=function(idComponente){
+recuperarTexto = function(idComponente){
     let conmponente;
     let valorIngresado;
     conmponente=document.getElementById(idComponente);
     valorIngresado=conmponente.value; 
     return valorIngresado;
+}
+recuperarInt = function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat = function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorDecimal=parseFloat(valorCaja);
+    return valorDecimal
 }
