42dd7f251ef39df62b3544ecd51b0fcace55ca56
LUIS-DAVID-04 <floreshurtadoluisdavid9@gmail.com>
2025-09-23 09:41:36 -0500

    Se separa las funciones al archivo utilitarios.js

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -15,37 +15,3 @@ saludar = function(){
 
     mostrarTextoEnCaja("txtNombre"," ");
 }
-mostrarTexto = function(idComponente,mensaje){
-    let conmponente;
-    conmponente=document.getElementById(idComponente);
-    conmponente.innerText = mensaje;
-
-}
-mostrarTextoEnCaja = function(idComponente,mensaje){
-    let conmponente;
-    conmponente=document.getElementById(idComponente);
-    conmponente.value = mensaje;
-}
-mostrarImgane = function(idComponente,rutaImg){
-    let conmponente;
-    conmponente=document.getElementById(idComponente);
-    conmponente.src = rutaImg;
-}
-recuperarTexto = function(idComponente){
-    let conmponente;
-    let valorIngresado;
-    conmponente=document.getElementById(idComponente);
-    valorIngresado=conmponente.value; 
-    return valorIngresado;
-}
-recuperarInt = function(idComponente){
-    let valorCaja=recuperarTexto(idComponente);
-    let valorEntero=parseInt(valorCaja);
-    return valorEntero;
-}
-
-recuperarFloat = function(idComponente){
-    let valorCaja=recuperarTexto(idComponente);
-    let valorDecimal=parseFloat(valorCaja);
-    return valorDecimal
-}
