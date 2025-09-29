3e3421d2f30e991d4f42921fa0eac6b87f9431f1
LUIS-DAVID-04 <floreshurtadoluisdavid9@gmail.com>
2025-09-29 11:29:29 -0500

    Promedio de notas mejorado

--- Promedio de notas/promedioNotas.js
+++ Promedio de notas/promedioNotas.js
@@ -4,11 +4,22 @@ calcularPromedioNotas = function(){
     let nota3 = recuperarFloat("txtnota3");
     let promedio=calcularPromedio(nota1,nota2,nota3);
     let promedioFlotante = promedio.toFixed(2);
-    if(promedioFlotante>7){
-        mostrarImgane("img","./imagenes/pasaste.gif");
-        
-    }else{
+    if(promedioFlotante < 5 && promedioFlotante > 0){
+        let mensaje = "REPROBADO";
+        mostrarTexto("txtRespuesta",mensaje);
         mostrarImgane("img","./imagenes/reprobado.gif");
+        
+    }else if (promedioFlotante >= 5 && promedioFlotante <= 8){
+        let mensaje = "BUEN TRABAJO";
+        mostrarTexto("txtRespuesta",mensaje);
+        mostrarImgane("img","./imagenes/bueno.gif");
+    }else if (promedioFlotante > 8 && promedioFlotante <=10){
+        let mensaje = "EXCELENTE";
+        mostrarTexto("txtRespuesta",mensaje);
+        mostrarImgane("img","./imagenes/pasaste.gif");
+    }else {
+        let mensaje = "DATOS INCORRECTOS";
+        mostrarTexto("txtRespuesta",mensaje);
+        mostrarImgane("img","./imagenes/error.gif");
     }
-
 }
